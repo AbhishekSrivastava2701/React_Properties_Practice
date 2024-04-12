0509b467d40bdb6a982759f0696d6acad4e305d8
Abhishek Srivastava <113105622+AbhishekSrivastava2701@users.noreply.github.com>
2024-04-12 22:31:51 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Card from "./Card";
+function App() {
+  return (
+    <div>
+      <h1 className="heading"><u> My Contact Card</u></h1>
+      <Card name="Abhishek Srivastava"
+        img="https://media.gettyimages.com/id/1249148401/photo/indian-sports-honours-in-mumbai.jpg?s=612x612&w=gi&k=20&c=wg1CX29KqoyZ_SK-hVKK_fAFzQb8ybLe_PpPdq4JNE8="
+        tel="+91 8383881274"
+        email="abhisheksrivastava2525@gmail..com"
+        id="abhishek-srivastava-a69a09249"
+      />
+    </div>
+  );
+}
+
+export default App;
